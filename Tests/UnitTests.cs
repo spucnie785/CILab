e530868bab1c9772230c0b0ca0610d7bbbc478fb
spucnie785 <111819544+spucnie785@users.noreply.github.com>
2024-04-12 19:54:35 -0500

    Update UnitTests.cs

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -33,7 +33,7 @@ namespace GithubActionsLab
          [Test]
         public void Subtract_Valid()
         {
-            Assert.AreEqual(1, Program.Subtract("2", "2"));
+            Assert.AreEqual(0, Program.Subtract("2", "2"));
             Assert.AreEqual(1, Program.Subtract("3", "2"));
             Assert.AreEqual(2, Program.Subtract("5", "3"));
         }
