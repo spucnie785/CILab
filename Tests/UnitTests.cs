6597e2fefa8a75aef3c7e4d6359c7fcaadf41fff
spucnie785 <111819544+spucnie785@users.noreply.github.com>
2024-04-12 19:57:08 -0500

    Update UnitTests.cs

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -77,28 +77,7 @@ namespace GithubActionsLab
             Assert.Throws<ArgumentNullException>(() => Program.Multiply(null, null));
         }
 
-         [Test]
-        public void Divide_Valid()
-        {
-            Assert.AreEqual(1, Program.Divide("2", "2"));
-            Assert.AreEqual(6, Program.Divide("3", "2"));
-            Assert.AreEqual(15, Program.Divide("5", "3"));
-        }
-
-        [Test]
-        public void Divide_Invalid()
-        {
-            Assert.Throws<FormatException>(() => Program.Divide("1", "a"));
-            Assert.Throws<FormatException>(() => Program.Divide("a", "1"));
-            Assert.Throws<FormatException>(() => Program.Divide("a", "a"));
-        }
-
-        [Test]
-        public void Divide_Null()
-        {
-            Assert.Throws<ArgumentNullException>(() => Program.Divide("1", null));
-            Assert.Throws<ArgumentNullException>(() => Program.Divide(null, "1"));
-            Assert.Throws<ArgumentNullException>(() => Program.Divide(null, null));
+        ntNullException>(() => Program.Divide(null, null));
         }
         
     }
